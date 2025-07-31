11e5e6b88948d45d00b0d4db2519adc39fd3e22a
haeyun0911 <haeyun0911@gmail.com>
2025-07-31 16:24:53 +0900

    0

---
+++ src/blur_avg_kernel.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../img/gaussian_noise.jpg')
+
+kernel =np.ones((5,5))/5**2
+blured = cv2.filter2D(img, -1, kernel)
+
+cv2.imshow('origin', img)
+cv2.imshow('avrg blur', blured) 
+cv2.waitKey()
+cv2.destroyAllWindows()
